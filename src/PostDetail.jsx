a5b40ddbf3597ccaf2e8159d679d0dae294845cc
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2023-11-22 23:10:12 +0900

    PostDetail comment
    PostDetail comment
    usequery로 불러오기

--- src/PostDetail.jsx
+++ src/PostDetail.jsx
@@ -1,3 +1,5 @@
+import { useQuery } from "react-query";
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -23,7 +25,20 @@ async function updatePost(postId) {
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
+
+  const { data, isError, error, isLoading } = useQuery("posts", post.id);
+
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  } else if (isError) {
+    return (
+      <>
+        <h3>
+          Oops, something went wrong.. <p>{error.toString}</p>
+        </h3>
+      </>
+    );
+  }
 
   return (
     <>
